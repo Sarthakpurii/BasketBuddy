1a6db07313e51a9021a2f90f2a54a6a7c39bdf37
Sarthak <sarthak.00puri@gmail.com>
2024-02-05 14:16:09 +0530

    added data

---
+++ lib/screens/home-screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('BasketBuddy'),),
+      body: Text('Hello World'),
+    );
+  }
+}
