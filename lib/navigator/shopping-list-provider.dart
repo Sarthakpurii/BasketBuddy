83d6a55715b9d1d0f1c038df5d25e04a396fb7bb
Sarthak <sarthak.00puri@gmail.com>
2024-02-09 23:15:26 +0530

    added shopping data provider

---
+++ lib/navigator/shopping-list-provider.dart
@@ -0,0 +1,12 @@
+import 'package:basketbuddy/models/grocery-item.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ShoppingListNotifier extends StateNotifier<Map<String,List<GroceryItem>>>{
+  ShoppingListNotifier():super({'Morning':[],'Tomorrow':[]});
+
+  void addItem(String title,GroceryItem item){
+    state={...state,title:state[title]!+[item]};
+  }
+}
+
+final shoppingListProvider=StateNotifierProvider((ref) => ShoppingListNotifier());
