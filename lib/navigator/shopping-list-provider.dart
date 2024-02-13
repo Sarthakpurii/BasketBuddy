dfa6ca40415dd24f839c38f7dbd05cfd65194973
Sarthak <sarthak.00puri@gmail.com>
2024-02-13 21:01:41 +0530

    update

--- lib/navigator/shopping-list-provider.dart
+++ lib/navigator/shopping-list-provider.dart
@@ -3,12 +3,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ShoppingListNotifier extends StateNotifier<Map<String,List<GroceryItem>>>{
   ShoppingListNotifier():super({'Morning':[],'Tomorrow':[]});
-
   void addItem(String title,GroceryItem item){
     state={...state,title:state[title]!+[item]};
   }
-  
-
 }
 
 final shoppingListProvider=StateNotifierProvider((ref) => ShoppingListNotifier());
